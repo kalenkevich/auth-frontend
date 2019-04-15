f823d5fb75d0614de29c1c81ab748051e382250b
Alexey Kalenkevich <alexei.kalenkevich@talenya.com>
2019-04-15 18:05:39 +0300

    Organize settings

--- config/settings/default.js
+++ config/settings/default.js
@@ -1,15 +1,17 @@
+const origin = window.location.origin;
+
 export default {
   AppName: 'Auth',
   TwitterAppId: 'o61j1KfJuizWXmGJbSFhwNnNa',
-  TwitterAppRedirectUrl: 'http://localhost:8082/social/twitter/callback',
+  TwitterAppRedirectUrl: `${origin}/social/twitter/callback`,
   FacebookAppId: '2624843640924018',
-  FacebookAppRedirectUrl: 'http://localhost:8082/social/facebook/callback',
+  FacebookAppRedirectUrl: `${origin}/social/facebook/callback`,
   LinkedInAppId: '77teax3v9i5w46',
-  LinkedInAppRedirectUrl: 'http://localhost:8082/social/linkedin/callback',
+  LinkedInAppRedirectUrl: `${origin}/social/linkedin/callback`,
   VkAppId: '6942977',
-  VkAppRedirectUrl: 'http://localhost:8082/social/vk/callback',
+  VkAppRedirectUrl: `${origin}/social/vk/callback`,
   InstagramAppId: 'be3112c86a1449f1877023263dafe3bc',
-  InstagramAppRedirectUrl: 'http://localhost:8082/social/instagram/callback',
+  InstagramAppRedirectUrl: `${origin}/social/instagram/callback`,
   GoogleAppId: '814016031269-105vip495ib3q2usb4jq3htb09054os0.apps.googleusercontent.com',
-  GoogleAppRedirectUrl: 'http://localhost:8082/social/google/callback',
+  GoogleAppRedirectUrl: `${origin}/social/google/callback`,
 };
